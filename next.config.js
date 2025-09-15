f36061199653cd5249c05c4f063b395c35263529
Pittbyte882 <pittbyte82@gmail.com>
2025-09-14 18:39:21 -0700

    Remove static export for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',         // Required for next export
-  trailingSlash: true,      // Optional: adds / to the end of URLs (e.g., /about/)
-  images: {
-    unoptimized: true       // Disable Image Optimization for export
-  }
+ 
 };
 
 module.exports = nextConfig;
